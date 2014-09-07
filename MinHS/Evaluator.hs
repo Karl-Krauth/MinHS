10cad6d8fbdda6de1ccd02c819c287d4f45cb6d3
Karl Krauth <karl.krauth@gmail.com>
2014-09-07 21:51:39 +1000

    Added support for recursion + functions with no arguments

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -11,18 +11,34 @@ data Value = I Integer
            | B Bool
            | Nil
            | Cons Integer Value
-           | InfL
+           | BinL
            | UnaL Integer
-           | InfA (Integer->Integer->Integer)
+           | BinA (Integer->Integer->Integer)
            | UnaA (Integer->Integer) 
-           | InfD (Integer->Integer->Integer)
+           | BinD (Integer->Integer->Integer)
            | UnaD (Integer->Integer)
-           | InfC (Integer->Integer->Bool)
+           | BinC (Integer->Integer->Bool)
            | UnaC (Integer->Bool)
            | Hea
            | Tai
            | Nul
-           | Cl [Id] Exp VEnv
+           | Cl Id [Id] Exp VEnv
+
+showVal :: Value -> String
+showVal (I i)    = "I " ++ (show i)
+showVal (B b)      = "B " ++ (show b)
+showVal Nil      = "Nil" 
+showVal (Cons i v) = "Cons " ++ (show i) ++ " " ++ (showVal v)
+showVal BinL     = "BinL" 
+showVal (UnaL i)   = "UnaL " ++ show i 
+showVal (BinA f)   = "BinA" 
+showVal (UnaD f)   = "UnaD" 
+showVal (BinC f)     = "BinC" 
+showVal (UnaC f)     = "UnaC"
+showVal Hea      = "Hea"
+showVal Tai      = "Tai"
+showVal Nul      = "Nul"
+showVal (Cl i is e g) = "Cl " ++ i ++ (show is) ++ (show e)
 
 instance PP.Pretty Value where
   pretty (I i) = numeric $ i
@@ -38,59 +54,61 @@ evaluate bs = evalE E.empty (Let bs (Var "main"))
 evalE :: VEnv -> Exp -> Value
 --evalE g e = error (show e) --Warning: for testing only
 evalE g (Var id) 
-  | isJust res = fromJust res
-  | otherwise = error ("Undefined variabe " ++ id)
-  where res = E.lookup g id
+  | isJust res                    = fromJust res
+  | otherwise                     = error ("Undefined variabe " ++ id)
+  where res                       = E.lookup g id
 evalE g (Prim op)
-  | isArith op = InfA (arithToFunc op)
-  | op == Neg  = UnaA negate
-  | op == Quot = InfD quot
-  | op == Rem  = InfD rem
-  | isComp op  = InfC (compToFunc op)
-  | op == Head = Hea
-  | op == Tail = Tai
-  | op == Null = Nul
+  | isArith op                    = BinA (arithToFunc op)
+  | op == Neg                     = UnaA negate
+  | op == Quot                    = BinD quot
+  | op == Rem                     = BinD rem
+  | isComp op                     = BinC (compToFunc op)
+  | op == Head                    = Hea
+  | op == Tail                    = Tai
+  | op == Null                    = Nul
 evalE g (Con id)
-  | id == "True"  = B True
-  | id == "False" = B False
-  | id == "Nil"   = Nil
-  | id == "Cons"  = InfL
-evalE _ (Num a) = I a
-evalE g (App e1 e2) = evalA (evalE g e1) (evalE g e2)
+  | id == "True"                  = B True
+  | id == "False"                 = B False
+  | id == "Nil"                   = Nil
+  | id == "Cons"                  = BinL
+evalE _ (Num a)                   = I a
+evalE g (App e1 e2)               = evalA (evalE g e1) (evalE g e2)
 evalE g (If e1 e2 e3)  
-  | getBool c      = evalE g e2
-  | otherwise      = evalE g e3
-  where c     = evalE g e1
-evalE g (Let [] e) = evalE g e
-evalE g (Let (b:bs) e) = evalE (evalB g b) (Let bs e)
-evalE g (Letfun (Bind i t ids e)) = Cl ids e g
-evalE g e = error (show e)
+  | getBool c                     = evalE g e2
+  | otherwise                     = evalE g e3
+  where c                         = evalE g e1
+evalE g (Let [] e)                = evalE g e
+evalE g (Let (b:bs) e)            = evalE (evalB g b) (Let bs e)
+--evalE g (Letfun (Bind f t [] e))  = evalE (E.add g (f, Cl f [] e g)) e
+evalE g (Letfun (Bind f t ids e)) = Cl f ids e g
+evalE g e                         = error (show e)
 
 
 evalB :: VEnv -> Bind -> VEnv
 evalB g (Bind i t [] e)  = E.add g (i, evalE g e)
-evalB g (Bind i t ids e) = E.add g (i, Cl ids e g)
+evalB g (Bind i t ids e) = E.add g (i, Cl "" ids e g)
 
 
 evalA :: Value -> Value -> Value
-evalA (Cl [] e g) v           = evalE g e
-evalA (Cl [i] e g) v          = evalE (E.add g (i, v)) e
-evalA (Cl (i:is) e g) v       = Cl is e (E.add g (i, v))
-evalA InfL (I i)              = UnaL i
+evalA c@(Cl f [i] e g) v      = evalE env e
+  where env = E.add (E.add g (i, v)) (f, c) 
+evalA (Cl f (i:is) e g) v     = Cl f is e (E.add g (i, v))
+evalA BinL (I i)              = UnaL i
 evalA (UnaL i) v              = Cons i v
-evalA (InfA f) (I i)          = UnaA (f i)
+evalA (BinA f) (I i)          = UnaA (f i)
 evalA (UnaA f) (I i)          = I (f i)
-evalA (InfD f) (I i)          = UnaD (f i)
+evalA (BinD f) (I i)          = UnaD (f i)
 evalA (UnaD f) (I i) 
   | i == 0                    = error "Division by zero"
   | otherwise                 = I (f i)
-evalA (InfC f) (I i)          = UnaC (f i)
+evalA (BinC f) (I i)          = UnaC (f i)
 evalA (UnaC f) (I i)          = B (f i)
 evalA Hea (Cons i v)          = I i 
 evalA Tai (Cons i v)          = v
 evalA Nul (Cons i v)          = B False
 evalA Nul Nil                 = B True
-evalA _ _                     = error "Invalid function application."
+evalA v c@(Cl f [] e g)       = evalA v (evalE (E.add g (f, c)) e)
+evalA v1 v2                   = error "Invalid function application."
 
 
 getBool :: Value -> Bool
